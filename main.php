b28fb88fb3f24357d9c089ad770cc194f32287d9
Taylor Oyer <giantsoup@users.noreply.github.com>
2025-01-04 02:26:11 -0800

    Add files via GitHub
    Added main file

---
+++ main.php
@@ -0,0 +1,98 @@
+<?php
+
+$fighterONE = [
+    'initiative' => 1,
+    'weapon' => [
+        'normal' => 3,
+        'critical' => 4
+    ],
+    'health' => 8
+];
+
+$fighterTWO = [
+    'initiative' => 0,
+    'weapon' => [
+        'normal' => 3,
+        'critical' => 4
+    ],
+    'health' => 4
+];
+
+$fighterONE['rolls'] = [
+    'hits' => 2,
+    'crits' => 0
+];
+
+$fighterTWO['rolls'] = [
+    'hits' => 1,
+    'crits' => 0
+];
+
+/*
+
+RESOLUTIONS
+
+Fighters take turns resolving their Hits/Crits. Fighter with initiative goes first.
+
+Round 1
+F1 Attacks, using a Hit
+F1 Hits = 1
+F2 takes 3 damage
+F2 Health = 1
+
+Round 2
+F2 Attacks, using a Hit
+F2 Hits = 0
+F1 takes 3 damage
+F1 Health = 5
+
+Round 3
+F1 Attacks, using a Hit
+F1 Hits = 0
+F2 takes 3 damage
+F2 Health = 0
+
+Result
+F1 Health = 5
+F2 Health = 0 (dead)
+
+----------------------------------
+
+Round 1
+F1 Attacks, using a Hit
+F1 Hits = 1
+F2 takes 3 damage
+F2 Health = 1
+
+Round 2
+F2 Blocks, using a Hit and removing an Unresolved Hit from F1
+F2 Hits = 0
+F1 Hits = 0
+
+Result
+F1 Health = 5
+F2 Health = 1
+
+----------------------------------
+
+Round 1
+F1 Blocks, using a Hit and removing an Unresolved Hit from F2
+F1 Hits = 1
+F2 Hits = 0
+
+Round 2
+F1 Attacks, using a Hit
+F1 Hits = 0
+F2 Health = 1
+
+Result
+F1 Health = 8
+F2 Health = 1
+
+
+In this case, a simple algorithm would have Fighter One choose the first scenario since that leaves their opponent dead.
+However, Fighter Two also knows this and will most likely choose to block with their Hit to avoid dying.
+In that case the best outcome for Fighter One is the last scenario, dealing damage and taking no damage.
+
+ */
+
